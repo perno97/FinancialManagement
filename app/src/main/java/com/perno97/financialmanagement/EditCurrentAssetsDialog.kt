0163eac2f2da861ae8325a5c140dc71af452e257
perno97 <fabio.perini97@gmail.com>
2022-11-02 17:39:53 +0100

    Updated main activity. Added "edit assets" dialog layout and class.

---
+++ app/src/main/java/com/perno97/financialmanagement/EditCurrentAssetsDialog.kt
@@ -0,0 +1,11 @@
+package com.perno97.financialmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditCurrentAssetsDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_current_assets_dialog)
+    }
+}

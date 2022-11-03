4688021ca15ca7441b2ed4f8d52d330ee3678494
perno97 <fabio.perini97@gmail.com>
2022-11-03 12:56:06 +0100

    Launching Main Activity

--- app/src/main/java/com/perno97/financialmanagement/MainActivity.kt
+++ app/src/main/java/com/perno97/financialmanagement/MainActivity.kt
@@ -2,10 +2,24 @@ package com.perno97.financialmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.perno97.financialmanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.txtCurrentValue.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setView(R.layout.activity_edit_current_assets_dialog)
+            val dialog = builder.create()
+        }
+
     }
 }
